7f73d5f99556c16eddc41b66ecec75c4be4f2173
생각하는 마리오네트 <66814045+ljs7463@users.noreply.github.com>
2021-12-24 14:15:10 +0900

    2주차 문제4

--- SQL/week2.sql
+++ SQL/week2.sql
@@ -6,13 +6,26 @@ inner join address a on c.address_id = a.address_id;
 
 -- 문제2번) 고객의  기본 정보인, 고객 id, 이름, 성, 이메일과 함께 고객의 주소 address, district, postal_code, phone , city 를 함께 알려주세요.
 
+select c.customer_id, c.first_name, c.last_name, c.email,  a.address, a.district, a.postal_code, a.phone, c2.city 
+from customer c 
+inner join address a on c.address_id  =  a.address_id 
+inner join city c2 on a.city_id = c2.city_id; 
+
 -- 문제3번) Lima City에 사는 고객의 이름과, 성, 이메일, phonenumber에 대해서 알려주세요.
 
-문제4번) rental 정보에 추가로, 고객의 이름과, 직원의 이름을 함께 보여주세요.
+select c2.first_name , c2.last_name, c2.email, a.phone from city c
+inner join address a  on c.city_id = a.city_id 
+inner join customer c2  on a.address_id = c2.address_id 
+where c.city like '%Lima%';
 
-- 고객의 이름, 직원 이름은 이름과 성을 fullname 컬럼으로만들어서 직원이름/고객이름 2개의 컬럼으로 확인해주세요.
+-- 문제4번) rental 정보에 추가로, 고객의 이름과, 직원의 이름을 함께 보여주세요.(고객의 이름, 직원 이름은 이름과 성을 fullname 컬럼으로만들어서 직원이름/고객이름 2개의 컬럼으로 확인해주세요.)
 
-문제5번) [seth.hannon@sakilacustomer.org](mailto:seth.hannon@sakilacustomer.org) 이메일 주소를 가진 고객의  주소 address, address2, postal_code, phone, city 주소를 알려주세요.
+select r.*, c.first_name||',' ||c.last_name as fullname,  s.first_name ||','||s.last_name as fullname
+from rental r
+left join customer c on r.customer_id = c.customer_id 
+left join staff s on c.address_id = s.address_id;
+
+-- 문제5번) [seth.hannon@sakilacustomer.org](mailto:seth.hannon@sakilacustomer.org) 이메일 주소를 가진 고객의  주소 address, address2, postal_code, phone, city 주소를 알려주세요.
 
 문제6번) Jon Stephens 직원을 통해 dvd대여를 한 payment 기록 정보를  확인하려고 합니다.
 - payment_id,  고객 이름 과 성,  rental_id, amount, staff 이름과 성을 알려주세요.
