a21ad99742c13d619b607301b0915de95abd3810
생각하는 마리오네트 <66814045+ljs7463@users.noreply.github.com>
2021-12-27 17:35:29 +0900

    12/27 끝

--- SQL/week2.sql
+++ SQL/week2.sql
@@ -66,13 +66,24 @@ left join address a on c2.city_id  = a.city_id
 left join customer c3 on a.address_id = c3.address_id 
 where c.country not in ('china');
 
-문제11번) Horror 카테고리 장르에 해당하는 영화의 이름과 description 에 대해서 알려주세요
+-- 문제11번) Horror 카테고리 장르에 해당하는 영화의 이름과 description 에 대해서 알려주세요
 
-문제12번) Music 장르이면서, 영화길이가 60~180분 사이에 해당하는 영화의 title, description, length 를 알려주세요.
+select f.title, f.description from film f 
+left join film_category fc on f.film_id =fc.film_id 
+left join category c on fc.category_id = c.category_id 
+where c."name" ='Horror';
 
-- 영화 길이가 짧은 순으로 정렬해서 알려주세요.
+-- 문제12번) Music 장르이면서, 영화길이가 60~180분 사이에 해당하는 영화의 title, description, length 를 알려주세요.(영화 길이가 짧은 순으로 정렬해서 알려주세요.)
 
-문제13번) actor 테이블을 이용하여,  배우의 ID, 이름, 성 컬럼에 추가로    'Angels Life' 영화에 나온 영화 배우 여부를 Y , N 으로 컬럼을 추가 표기해주세요.  해당 컬럼은 angelslife_flag로 만들어주세요.
+select f.title, f.description, f.length from film f 
+inner join film_category fc on f.film_id = fc.film_id 
+inner join category c on fc.category_id = c.category_id 
+where (c."name" ='Music') and (f.length between 60 and 180)
+order by f.length;
+
+-- 문제13번) actor 테이블을 이용하여,  배우의 ID, 이름, 성 컬럼에 추가로    'Angels Life' 영화에 나온 영화 배우 여부를 Y , N 으로 컬럼을 추가 표기해주세요.  해당 컬럼은 angelslife_flag로 만들어주세요.
+
+
 
 문제14번) 대여일자가 2005-06-01~ 14일에 해당하는 주문 중에서 , 직원의 이름(이름 성) = 'Mike Hillyer' 이거나  고객의 이름이 (이름 성) ='Gloria Cook'  에 해당 하는 rental 의 모든 정보를 알려주세요.
 
