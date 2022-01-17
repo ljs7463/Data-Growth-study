28ca7f071e90e5f07f964480836a06d3bb1328ae
생각하는 마리오네트 <66814045+ljs7463@users.noreply.github.com>
2022-01-17 21:30:12 +0900

    sql

--- SQL/week3.sql
+++ SQL/week3.sql
@@ -23,7 +23,21 @@ where exists (
 )
 
 -- 문제3번) 대여가 한번도이라도 된 영화 카테 고리 이름을 알려주세요. (쿼리는, Any 조건을 이용하여 풀어봅시다)
+select c.name
 
+from category as c
+
+where category_id = any (
+
+select fc.category_id
+
+from rental as r
+
+join inventory as i on r.inventory_id = i.inventory_id
+
+join film_category as fc on i.film_id = fc.film_id
+
+)
 
 -- 문제4번) 대여가 가장 많이 진행된 카테고리는 무엇인가요? (Any, All 조건 중 하나를 사용하여 풀어봅시다)
 
