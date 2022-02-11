92d15ffd7825672de4d6b36cfae6fe6ce16079ad
생각하는 마리오네트 <66814045+ljs7463@users.noreply.github.com>
2022-02-11 22:25:18 +0900

    가독성 증가를 위한 로직수정

--- SQL/week3.sql
+++ SQL/week3.sql
@@ -1,4 +1,8 @@
-< 실습문제 3-6 >
+/*
+모든 문제는 postgresql기준으로 작성되어있습니다.
+*/
+
+-- < 실습문제 3-6 >
 
 --문제1번) 매출을 가장 많이 올린 dvd 고객 이름은? (subquery 활용)
 select first_name , last_name 
@@ -24,19 +28,10 @@ where exists (
 
 -- 문제3번) 대여가 한번도이라도 된 영화 카테 고리 이름을 알려주세요. (쿼리는, Any 조건을 이용하여 풀어봅시다)
 select c.name
-
 from category as c
-
-where category_id = any (
-
-select fc.category_id
-
-from rental as r
-
-join inventory as i on r.inventory_id = i.inventory_id
-
-join film_category as fc on i.film_id = fc.film_id
-
+where category_id = any ( select fc.category_id from rental as r
+			join inventory as i on r.inventory_id = i.inventory_id
+			join film_category as fc on i.film_id = fc.film_id
 )
 
 -- 문제4번) 대여가 가장 많이 진행된 카테고리는 무엇인가요? (Any, All 조건 중 하나를 사용하여 풀어봅시다)
@@ -208,11 +203,11 @@ from (
        select mm , orderdate , sum(product_price) product_price
        from 
                (
-                        select o.ordernumber, orderdate, EXTRACT(month from o.orderdate) as mm, 
+		select o.ordernumber, orderdate, EXTRACT(month from o.orderdate) as mm, 
                                    od.productnumber, od.quotedprice * od.quantityordered as product_price
-                        from orders as o 
-                             join order_details as od  on o.ordernumber= od.ordernumber 
-                        where o.orderdate between '2017-01-01' and '2017-12-31'
+		from orders as o 
+		     join order_details as od  on o.ordernumber= od.ordernumber 
+		where o.orderdate between '2017-01-01' and '2017-12-31'
                    ) dt  
                 group by mm , orderdate
 ) as dt 
